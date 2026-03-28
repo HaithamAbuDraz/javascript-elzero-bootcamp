0842bbc24d7c6e1847ebb05ec72e5e1038f1bb2f
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 13:36:29 +0300

    Add main.js file for String Syntax and Characters Escape Sequences lesson

---
+++ Lessons/Week 2/9. String Syntax And Characters Escape Sequences/main.js
@@ -0,0 +1,17 @@
+/*
+  String Syntax + Character Escape Sequences
+  \ Escape + Line Continu
+  \n New Line
+*/
+
+console.log("Haitham \"Abu Draz\""); // Haitham "Abu Draz"
+console.log("Haitham \'Abu Draz\'"); // Haitham 'Abu Draz'
+console.log("Haitham Abu Draz"); // Haitham Abu Draz
+console.log("Haitham \\Abu Draz"); // Haitham \Abu Draz
+console.log("Haitham \
+Abu \
+Draz"); // Haitham Abu Draz
+console.log("Haitham \nAbu \nDraz");
+// Haitham
+// Abu
+// Draz
