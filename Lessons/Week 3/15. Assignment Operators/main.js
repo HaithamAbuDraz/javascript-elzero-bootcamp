e495923bc7a841238c6d07b256757e228ff5d3ec
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 17:51:06 +0300

    Add main.js file for Assignment Operators lesson

---
+++ Lessons/Week 3/15. Assignment Operators/main.js
@@ -0,0 +1,12 @@
+/*
+  Assignment Operators
+*/
+
+let a = 10;
+
+a = a + 20; // 30
+a = a + 70; // 100
+a += 100; // a = a + 100
+a -= 50; // a = a - 50
+a /= 50; // a = a / 50;
+console.log(a); // 3
