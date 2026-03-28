1cd33037be6f4499e105325d683801dec4b303ce
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 13:14:58 +0300

    Add main.js file for Variables Introduction lesson

---
+++ Lessons/Week 2/6. Variables Introduction/main.js
@@ -0,0 +1,22 @@
+/*
+  Variables Intro
+  - What is Variable ?
+  - Why We Use Variables ?
+  - Declare A Variables ?
+  - Syntax ( keyword | Variable Name | Assignment Operator | Variable Value )
+  - Variable Without Var
+  - Multiple Variables in The Same Line
+  - Id And Global Variable
+  - Loosely Typed vs Strongly Typed
+*/
+
+var user = "Haitham",
+    age = 21;
+
+console.log(user); // Haitham
+console.log(user); // Haitham
+console.log(user); // Haitham
+console.log(age); // 21
+console.log(hello); // <div id="hello">Hello</div>
+
+hello.innerHTML = "Option"; // <div id="hello">Option</div>
