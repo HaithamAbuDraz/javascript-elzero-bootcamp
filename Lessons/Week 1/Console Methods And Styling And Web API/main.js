17458f05d271db3630054ddd7bfbc7076a308f46
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 11:19:02 +0300

    Add main.js file

---
+++ Lessons/Week 1/Console Methods And Styling And Web API/main.js
@@ -0,0 +1,21 @@
+/*
+  Console Methods
+  - log
+  - error
+  - table
+
+  Web API(Application Programming Interface)
+
+  Styling Console
+  - Directive %c
+*/
+
+console.log("Log"); // Log
+console.error("Error"); // Error
+console.table(["Haitham", "Mohammed", "Hasan"]);
+// (index) Value
+// 0	'Haitham'
+// 1	'Mohammed'
+// 2	'Hasan'
+
+console.log("Hello From %cJS %cFile", "color: red; font-size: 40px", "color: blue; font-size: 40px"); // Hello From JS File
