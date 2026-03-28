42d98f78e187a4a17fe5a02d2e7ec4dbeb5d65e5
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 12:45:58 +0300

    Add main.js file for Data Types and typeof Operator lesson

---
+++ Lessons/Week 2/5. Data Types And typeof Operator/main.js
@@ -0,0 +1,20 @@
+/*
+  Data Types Intro
+  - String
+  - Number
+  - Array => Object
+  - Object
+  - Boolean
+  - Undefined
+*/
+
+console.log(typeof "Haitham"); // String
+console.log(typeof 5000); // Number
+console.log(typeof 5000.99); // Number
+console.log(typeof [10, 15, 17]); // Object
+console.log(typeof ["Haitham", "Mohammed", "Hasan"]); // Object
+console.log(typeof { name: 'Haitham', age: 21, country: 'Ps' }); // Object
+console.log(typeof true); // Boolean
+console.log(typeof false); // Boolean
+console.log(typeof undefined); // Undefined
+console.log(typeof null); // Object (This is a bug in JavaScript, null should be of type null, but it is of type object)
