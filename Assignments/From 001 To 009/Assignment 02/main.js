1a7cf801cd63e29e57d2f4299ed3f7d20e6e86a8
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 11:55:55 +0300

    Add index.html and main.js files for Assignment 02

---
+++ Assignments/From 001 To 009/Assignment 02/main.js
@@ -0,0 +1,5 @@
+document.write("<h1>Elzero</h1>");
+let Elzero = document.querySelector("h1");
+Elzero.style.color = "blue";
+Elzero.style.font = "bold 80px Arial";
+Elzero.style.textAlign = "center";
