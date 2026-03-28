607eb3603feadfc6df9fd2b7595fd75b5b20dc70
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 12:25:00 +0300

    Add main.js file for Assignment 06

---
+++ Assignments/From 001 To 009/Assignment 06/main.js
@@ -0,0 +1,8 @@
+// First way we can comment the code with single line comment
+//console.log("Iam In Console");
+//document.write("Iam In Page");
+
+/* Second way we can comment the code with multi line comment
+console.log("Iam In Console");
+document.write("Iam In Page");
+*/
