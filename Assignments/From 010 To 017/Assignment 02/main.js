1c6ff8129474e657609a24a9425d2c7acceea7be
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 17:06:09 +0300

    Add main.js file for Assignment 02

---
+++ Assignments/From 010 To 017/Assignment 02/main.js
@@ -0,0 +1,7 @@
+// Assignment 2
+// Do not modify the JS file at all.
+// Do not write any JavaScript code anywhere to achieve the desired result.
+// You must ensure this code works and outputs the required result.
+
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
