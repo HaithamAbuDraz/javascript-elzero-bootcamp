ee185e67c0fd43876036f1caa9d708d71f813eb0
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 17:09:54 +0300

    Add main.js file for Assignment 03

---
+++ Assignments/From 010 To 017/Assignment 03/main.js
@@ -0,0 +1,12 @@
+// Assignment 03
+// Print the following result without using the Template Literals method to create Concatenate:
+/*
+`I'm In
+\\
+Love \\ """ '''
+++ With ++
+\"""\"""
+""JavaScript""``
+*/
+
+console.log("`I'm In\n\\\\\nLove \\\\ \"\"\" '''\n++ With ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
