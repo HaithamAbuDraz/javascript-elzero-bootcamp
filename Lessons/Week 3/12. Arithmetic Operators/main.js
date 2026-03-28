fb1cc4fa847cddbcc30b5bbe5e6e5e9d7554a1a7
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 17:30:54 +0300

    Add main.js file for Arithmetic Operators lesson

---
+++ Lessons/Week 3/12. Arithmetic Operators/main.js
@@ -0,0 +1,29 @@
+/*
+  Arithmetic Operators
+  + Addition
+  - Subtraction
+  * Multiplication
+  ** Exponentiation
+  / Division
+  % Modulus (Division Remainder)
+  ++ Increment [ Post | Pre ]
+  -- Decrement [ Post | Pre ]
+*/
+
+console.log(10 + 20); // 30
+console.log(10 + "Haitham"); // 10Haitham
+
+console.log(10 - 20); // -10
+console.log(10 - "Haitham"); // NaN: Not A Number
+console.log(typeof NaN); // number
+
+console.log(10 * 20); // 200
+console.log(10 * -20); // -200
+
+console.log(20 / 5); // 4
+console.log(20 / 3); // 6.666666666666667
+
+console.log(2 ** 4); // 16
+
+console.log(10 % 2); // 0
+console.log(11 % 2); // 1 Remove 1
