56eadbe58bc2773159d2f0f6ba39149ea53e5552
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 11:16:31 +0300

    Add main.js file

---
+++ Lessons/Week 1/Output To Screen/main.js
@@ -0,0 +1,12 @@
+/*
+  Output To Screen
+  - window.alert()
+  - document.write()
+  - console.log()
+
+*/
+window.alert("Hello from JS File");
+
+document.write("<h1>Hello <span>Page</span></h2>");
+
+console.log("Hello from JS File"); // Hello from JS File
