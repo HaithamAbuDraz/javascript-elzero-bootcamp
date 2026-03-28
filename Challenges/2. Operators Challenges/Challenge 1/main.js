d7379046b987172ec57a82a9880e8a76631daf9c
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-28 17:56:25 +0300

    Add main.js file for Operators Challenge 1

---
+++ Challenges/2. Operators Challenges/Challenge 1/main.js
@@ -0,0 +1,39 @@
+/*
+  Challenge 1
+*/
+
+/*
+  [++a] [+]
+  [++a]
+  - Value:
+  - Explain:
+  [+]
+  - Explain:
+*/
+
+let a = 10;
+let b = "20";
+let c = 80;
+
+// console.log(++a + b++ + c++ - a++); // 100
+// console.log(++a); // 11
+// console.log(b++); // 20
+// console.log(c++); // 80
+// console.log(a++); // 11
+
+// console.log(++a + -b + +c++ - -a++ + +a); // 94
+// console.log(++a); // 11
+// console.log(-b); // -20
+// console.log(+c++); // 80
+// console.log(-a++); // -11
+// console.log(+a); // 12
+
+// console.log(--c + +b + --a * +b++ - +b * a + --a - +true); // 97
+// console.log(--c); // 79
+// console.log(+b); // 20
+// console.log(--a); // 9
+// console.log(+b++); // 20
+// console.log(+b); // 21
+// console.log(a); // 9
+// console.log(--a); // 8
+// console.log(+true); // 1
