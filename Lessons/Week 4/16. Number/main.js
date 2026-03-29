316db3bbd173072820dc87d23f5990341bfe8bdb
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-29 13:50:22 +0300

    Add main.js file for Number lesson with examples and explanations

---
+++ Lessons/Week 4/16. Number/main.js
@@ -0,0 +1,21 @@
+/*
+  Number
+  - Double Precision
+  - Syntactic Sugar "_"
+  - e
+  - **
+  - With Decimal
+  - Number + BigInt
+  - Number Min Value
+  - Number Max Value
+*/
+
+console.log(1000000); // 1000000
+console.log(1_000_000); // 1000000
+console.log(1e6); // 1000000
+console.log(10 ** 6); // 1000000
+console.log(10 * 10 * 10 * 10 * 10 * 10); // 1000000
+console.log(1000000.0); // 1000000
+
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
