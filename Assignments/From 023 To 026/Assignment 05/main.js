fe086dead6e280ed8ffa8151be1185469d72d3b8
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-30 16:54:25 +0300

    Add main.js file for Assignment 05

---
+++ Assignments/From 023 To 026/Assignment 05/main.js
@@ -0,0 +1,12 @@
+/*
+Assignment 05
+The following variable contains the number 10.
+You must use `isInteger` to get the result 2.
+You are not allowed to enter any numbers yourself; you must use the previous property to get the required result.
+
+*/
+
+let num = 10;
+
+// Using Number.isInteger() returns true (1), then add two trues to get 2
+console.log(Number.isInteger(num) + Number.isInteger(num)); // 2
