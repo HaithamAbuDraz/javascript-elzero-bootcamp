77fec4c5e3a48a44efc273a9bb1e0996f17ca140
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-30 11:52:14 +0300

    Add main.js file for Assignment 02

---
+++ Assignments/From 018 To 022/Assignment 02/main.js
@@ -0,0 +1,29 @@
+/*
+Assignment 2
+Use the variable value to get the required result in six different ways.
+You can use anything you have learned previously to get the required result.
+Using any numbers is not allowed at all, but you can use Boolean values.
+Each method must be completely different from the others.
+The required result is the number 6.
+
+*/
+
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num * num - num); // 6
+
+// Solution Three
+console.log(num * (true + true)); // 6
+
+// Solution Four
+console.log(num ** (true + true) - num); // 6
+
+// Solution Five
+console.log(num + ++num - true); // 6
+
+// Solution Six
+console.log(num + true + true); // 6
