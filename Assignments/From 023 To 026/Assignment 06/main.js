9a30e7a4150eb2dd443c04b09d733a885beca4fd
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-30 17:00:35 +0300

    Add main.js file for Assignment 06

---
+++ Assignments/From 023 To 026/Assignment 06/main.js
@@ -0,0 +1,14 @@
+/*
+Assignment 6
+Use the following variable to obtain the result 10 in 5 different ways.
+Only the variable should be used; do not enter any numbers manually
+No solution should be duplicated.
+*/
+
+let flt = 10.4;
+
+console.log(Math.trunc(flt)); // 10
+console.log(Math.floor(flt)); // 10
+console.log(Math.round(flt)); // 10
+console.log(+flt.toFixed(0)); // 10
+console.log(parseInt(flt)); // 10
