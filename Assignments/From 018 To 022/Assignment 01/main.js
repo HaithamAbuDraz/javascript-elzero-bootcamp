b12f085ed75a2bc2bd30b29a424e8e941f882b0a
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-30 11:41:50 +0300

    Add main.js file for Assignment 01 with arithmetic operator challenge

---
+++ Assignments/From 018 To 022/Assignment 01/main.js
@@ -0,0 +1,11 @@
+/*
+Assignment 01
+Challenge
+Replace the question mark with one of the Arithmetic Operators to get the required result.
+No modifications to the numbers are allowed.
+The required result is zero.
+// Replace ? With Arithmetic Operators
+console.log(10 ? 20 ? 15 ? 3 ? 190 ? 10 ? 400); // 0
+*/
+
+console.log(10 * 20 * 15 % 3 * 190 * 10 * 400); // 0
