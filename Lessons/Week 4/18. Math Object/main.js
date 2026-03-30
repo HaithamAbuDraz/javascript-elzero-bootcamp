ed2c3056aecd22a7f020b25a8a5992a66606db81
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-30 15:02:13 +0300

    Add main.js file for Math Object lesson

---
+++ Lessons/Week 4/18. Math Object/main.js
@@ -0,0 +1,26 @@
+/*
+  Math Object
+  - round()
+  - ceil()
+  - floor()
+  - min()
+  - max()
+  - pow()
+  - random()
+  - trunc() [ES6]
+*/
+
+console.log(Math.round(99.2)); // 99
+console.log(Math.round(99.5)); // 100
+
+console.log(Math.ceil(99.2)); // 100
+console.log(Math.floor(99.8)); // 99
+
+console.log(Math.min(10, 20, 30, -50)); // -50
+console.log(Math.max(10, 20, 30, -50)); // 30
+
+console.log(Math.pow(2, 4)); // 16
+
+console.log(Math.random()); // Random number between 0 and 1
+
+console.log(Math.trunc(99.5)); // 99
