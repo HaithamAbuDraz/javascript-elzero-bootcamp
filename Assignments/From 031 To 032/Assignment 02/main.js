b9eecf3d9e0ea6e263f1a8caefad6113e6c2b4cc
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-31 17:21:12 +0300

    Add main.js file for Assignment 02

---
+++ Assignments/From 031 To 032/Assignment 02/main.js
@@ -0,0 +1,20 @@
+/*
+Assignment 2
+You have two variables containing two different numbers.
+You need to use the operators you've learned with these variables to get the result `True`.
+Using any numbers is strictly prohibited.
+The variable must be used only once in each solution.
+The operator must not be repeated and should only be used once in one of the solutions.
+The minimum number of solutions is 6.
+*/
+
+let num1 = 10;
+
+let num2 = 20;
+
+console.log(num1 < num2); // true
+console.log(num2 > num1); // true
+console.log(num1 != num2); // true
+console.log(num1 <= num2); // true
+console.log(num1 !== num2); // true
+console.log(!(num1 > num2)); // true
