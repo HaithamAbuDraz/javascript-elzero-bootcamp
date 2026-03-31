320358f7310ecee59539f8c60bef7f59f7186019
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-03-31 17:09:11 +0300

    Add main.js file for Assignment 01

---
+++ Assignments/From 031 To 032/Assignment 01/main.js
@@ -0,0 +1,13 @@
+/*
+Assignment 1
+Replace the question mark with the appropriate operator you have learned to make the result True.
+*/
+
+console.log(100 == "100"); // true
+console.log(100 !== 1000); // true
+console.log(110 > 100 && 10 < 20); // true
+console.log(-10 == "-10"); // true
+console.log(!(-50 >= +"-40")); // true
+console.log(!(10 >= -"-40")); // true
+console.log(!("10" != 10)); // true
+console.log(!(20 == false)); // true
