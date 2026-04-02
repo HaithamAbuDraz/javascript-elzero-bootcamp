5fd755028463eddbd93278deba11f436edcc89e6
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-02 14:49:46 +0300

    Add main.js file for Sorting Array

---
+++ Lessons/Week 6/33. Sorting Array/main.js
@@ -0,0 +1,11 @@
+/*
+  Arrays Methods [Sort]
+  - sort(Function [Opt])
+  - reverse
+*/
+
+let myFriends = [10, "Sayed", "Mohammed", "90", 1000, 100, 20, "10", -20, -10];
+console.log(myFriends);
+
+console.log(myFriends.sort()); // (10) [-10, -20, 10, '10', 100, 1000, 20, '90', 'Mohammed', 'Sayed']
+console.log(myFriends.reverse()); // (10) ['Sayed', 'Mohammed', '90', 20, 1000, 100, '10', 10, -20, -10]
