670d007057a86408045ce7ee62156dc2d62329f6
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-03 16:19:40 +0300

    Add main.js file for Assignment 02

---
+++ Assignments/From 040 To 047/Assignment 02/main.js
@@ -0,0 +1,14 @@
+/*
+Assignment 02
+Numbers are strictly prohibited.
+Slice Methods are prohibited.
+You can write whatever you want outside the console to complete the task.
+*/
+
+let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
+
+// Write Your Code Here
+friends.shift(); // Removes the first element from an array
+friends.pop(); // Removes the last element from an array
+
+console.log(friends); // ["Eman", "Osama"]
