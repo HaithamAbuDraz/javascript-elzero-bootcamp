ebe7ed77a97ee36ce39829d692d4679ad7c557ec
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-03 17:43:56 +0300

    Add main.js file for Assignment 04

---
+++ Assignments/From 040 To 047/Assignment 04/main.js
@@ -0,0 +1,11 @@
+/*
+Assignment 04
+Numbers are strictly prohibited; you may only use the number 0.
+Only one line of code must be written to generate the required value.
+*/
+
+let website = "Go";
+
+let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
+
+console.log(words[website.length][0].slice(website.length).toUpperCase()); // ZERO
