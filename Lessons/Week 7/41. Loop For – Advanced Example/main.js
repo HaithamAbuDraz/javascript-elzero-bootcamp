cecfec25d4bfa888ea8e337b7c1176ed59434d7f
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-04 14:32:48 +0300

    Add main.js file for Loop For – Advanced Example lesson

---
+++ Lessons/Week 7/41. Loop For – Advanced Example/main.js
@@ -0,0 +1,19 @@
+/*
+    Loop For Advanced Example
+*/
+
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor", "Iphone"];
+
+let i = 0;
+
+for (; ;) {
+    console.log(products[i]);
+    i++;
+    if (i === products.length) break;
+    // Keyboard
+    // Mouse
+    // Pen
+    // Pad
+    // Monitor
+    // Iphone
+}
