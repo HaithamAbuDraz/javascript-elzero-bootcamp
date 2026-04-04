c2b081cd3a8d7e2cd80e8f73303878a1c047b81f
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-04 15:44:59 +0300

    Add main.js file for Loop - Do While lesson

---
+++ Lessons/Week 7/44. Loop - Do While/main.js
@@ -0,0 +1,15 @@
+/*
+    Loop
+    - Do / While
+*/
+
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor", "iPhone"];
+
+let i = 0;
+
+do {
+    console.log(i); // 0
+    i++;
+} while (false);
+
+console.log(i); // 1
