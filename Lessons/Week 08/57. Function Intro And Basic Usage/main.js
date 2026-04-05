685d5b407736882e31ea8b09adc58daf7d301d00
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-05 16:05:15 +0300

    Add main.js file for Function Intro And Basic Usage lesson

---
+++ Lessons/Week 08/57. Function Intro And Basic Usage/main.js
@@ -0,0 +1,19 @@
+/*
+  Function
+  - What Is Function ?
+  - User Defined vs Bulit In
+  - Syntax + Basic Usage
+  - Example From Real Life
+  - Parameter + Argument
+  - Practical Example
+*/
+
+console.log(typeof console.log); // function
+
+function sayHello(userName) {
+  console.log(`Hello ${userName}`);
+}
+
+sayHello("Haitham"); // Hello Haitham
+sayHello("Sayed"); // Hello Sayed
+sayHello("Ali"); // Hello Ali
