742d03035f1d66fa1be13530e3605282f45fad86
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-06 16:20:13 +0300

    Add main.html file for Arrow Function Syntax lesson

---
+++ Lessons/Week 09/66. Arrow Function Syntax/main.js
@@ -0,0 +1,24 @@
+/*
+  Function
+  - Arrow Function
+  -- Regular vs Arrow [Param + No Param]
+  -- Multiple Lines
+*/
+
+// let print = function () {
+//   return 10;
+// };
+
+// let print = () => 10;
+
+// let print = function (num) {
+//   return num;
+// };
+
+// let print = num => num;
+
+let print = (num1, num2) => num1 + num2;
+
+// console.log(print(10)); // 10
+
+console.log(print(50, 100)); // 150
