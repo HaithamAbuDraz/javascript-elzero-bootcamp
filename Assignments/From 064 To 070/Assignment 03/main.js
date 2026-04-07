7b6eac8011427b37f79ac4a28477a6a4cbb3abb0
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-07 16:14:53 +0300

    Add main.js file for Assignment 03

---
+++ Assignments/From 064 To 070/Assignment 03/main.js
@@ -0,0 +1,23 @@
+/*
+Assignment 03
+Challenge
+Convert the following function to Arrow Function Syntax
+Read about Currying Function Technique
+*/
+
+// Regular Function
+function checker1(zName) {
+  return function (status) {
+    return function (salary) {
+      return status === "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Available`;
+    };
+  };
+}
+console.log(checker1("Osama")("Available")(4000)); // Osama, My Salary Is 4000
+console.log(checker1("Ahmed")("Not Available")()); // Iam Not Available
+
+// Arrow Function
+let checker2 = (zName) => (status) => (salary) =>
+  status === "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Available`;
+console.log(checker2("Osama")("Available")(4000)); // Osama, My Salary Is 4000
+console.log(checker2("Ahmed")("Not Available")()); // Iam Not Available
