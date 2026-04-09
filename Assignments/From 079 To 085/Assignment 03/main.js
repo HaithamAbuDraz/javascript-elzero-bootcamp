25211cf3736ca50a874bff073d252a06250517be
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-09 16:30:23 +0300

    Add main.js for Assignment 03

---
+++ Assignments/From 079 To 085/Assignment 03/main.js
@@ -0,0 +1,30 @@
+/*
+Assignment 03
+Use Object.assign() to create a new object containing all the above:
+*/
+
+let a = 1;
+
+let threeNums = {
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+let twoNums = {
+  e: 5,
+  f: 6,
+};
+
+// Create Your Object Here in One Line
+let finalObject = Object.assign({ a }, threeNums, twoNums);
+
+console.log(finalObject);
+/*
+a: 1
+b: 2
+c: 3
+d: 4
+e: 5
+f: 6
+*/
