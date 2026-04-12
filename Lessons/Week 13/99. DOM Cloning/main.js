0c581c156c06ff4fd016af8a218a0115e0475633
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-12 23:23:01 +0300

    Add main.js file for DOM Cloning lesson

---
+++ Lessons/Week 13/99. DOM Cloning/main.js
@@ -0,0 +1,10 @@
+/*
+  DOM [Cloning]
+  - cloneNode(Deep)
+*/
+
+let myP = document.querySelector("p").cloneNode(true);
+let myDiv = document.querySelector("div");
+
+myP.id = `${myP.id}-clone`
+myDiv.appendChild(myP);
