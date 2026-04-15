33770f031155aad23838797fccc98c8ce95819da
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-15 14:37:27 +0300

    Add main.js for Window Open And Close lesson

---
+++ Lessons/Week 14/107. Window Open And Close/main.js
@@ -0,0 +1,18 @@
+/*
+  BOM [Browser Object Model]
+  - open(URL [Opt], Window Name Or Target Attr [Opt], Win Features [Opt], History Replace [Opt])
+  - close()
+  - Window Features
+  --- left [Num]
+  --- top [Num]
+  --- width [Num]
+  --- height [Num]
+  --- menubar [yes || no]
+
+  Search
+  - Window.Open Window Features
+*/
+
+setTimeout(function () {
+  window.open("https://google.com", "_blank", "width=400, height=400, left=200, top=100");
+}, 2000);
