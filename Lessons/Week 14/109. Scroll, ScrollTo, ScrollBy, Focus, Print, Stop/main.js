9d6b8fec1a33a49a5aa31b46d7cfc2293e6aa8da
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-16 09:57:58 +0300

    Add main.js for Scroll, ScrollTo, ScrollBy, Focus, Print, Stop lesson

---
+++ Lessons/Week 14/109. Scroll, ScrollTo, ScrollBy, Focus, Print, Stop/main.js
@@ -0,0 +1,29 @@
+/*
+  BOM [Browser Object Model]
+  - stop()
+  - print()
+  - focus()
+  - scrollTo(x, y || Options)
+  - scroll(x, y || Options)
+  - scrollBy(x, y || Options)
+*/
+
+// window.print();
+
+// let myNewWindow = window.open("https://google.com", "", "width=500,height=500");
+
+// myNewWindow.focus();
+
+// myNewWindow.close();
+
+// window.scrollTo({
+//   left: 500,
+//   top: 200,
+//   behavior: "smooth"
+// });
+
+window.scrollBy({
+  left: 500,
+  top: 200,
+  behavior: "smooth"
+});
