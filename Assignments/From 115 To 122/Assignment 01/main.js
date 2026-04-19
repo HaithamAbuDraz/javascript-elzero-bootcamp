0a13861767ceb7f0c5286ffb66392c97aaef50ef
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-19 12:33:33 +0300

    Add main.js file for Assignment 01

---
+++ Assignments/From 115 To 122/Assignment 01/main.js
@@ -0,0 +1,11 @@
+/*
+Assignment 01
+You have an array containing numbers.
+Perform the Destructuring Assignment to obtain the desired result within the Console.
+*/
+
+let myNumbers = [1, 2, 3, 4, 5];
+
+let [a, , , , e] = myNumbers;
+
+console.log(a * e); // 5
