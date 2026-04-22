89c7dc3d06b06f6fcd94acb109d19236b3128895
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-22 11:21:31 +0300

    Add main.js file for Assignment 07

---
+++ Assignments/From 123 To 133/Assignment 07/main.js
@@ -0,0 +1,16 @@
+/*
+Assignment 07
+You have arrays that need to be merged.
+The task is to merge the arrays in three different ways.
+*/
+
+let numsOne = [1, 2, 3];
+let numsTwo = [4, 5, 6];
+
+console.log(numsOne.concat(numsTwo)); // [1, 2, 3, 4, 5, 6]
+
+console.log([...numsOne, ...numsTwo]); // [1, 2, 3, 4, 5, 6]
+
+numsOne.push(...numsTwo);
+
+console.log(numsOne); // [1, 2, 3, 4, 5, 6]
