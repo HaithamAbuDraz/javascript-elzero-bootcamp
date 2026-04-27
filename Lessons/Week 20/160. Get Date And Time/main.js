22072641d2029bfdcfe7a5f05d97c753fb12128c
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-27 15:11:33 +0300

    Add main.js file for Get Date And Time lesson

--- Lessons/Week 20/160. Get Date And Time/main.js
+++ Lessons/Week 20/160. Get Date And Time/main.js
@@ -17,11 +17,11 @@ let dateDiff = dateNow - birthday;
 console.log(dateDiff); // 690820014006
 console.log(dateDiff / 1000 / 60 / 60 / 24 / 365); // 21.905760773116437
 
-console.log(dateNow); // Mon Apr 27 2026 14:33:35 GMT+0300 (Eastern European Summer Time)
-console.log(dateNow.getTime()); // 1777289367033
+console.log(dateNow); // Mon Apr 27 2026 15:10:36 GMT+0300 (Eastern European Summer Time)
+console.log(dateNow.getTime()); // 1777291851594
 console.log(dateNow.getDate()); // 27
 console.log(dateNow.getFullYear()); // 2026
 console.log(dateNow.getMonth()); // 3
 console.log(dateNow.getHours()); // 14
-console.log(dateNow.getMinutes()); // 33
-console.log(dateNow.getSeconds()); // 35
+console.log(dateNow.getMinutes()); // 10
+console.log(dateNow.getSeconds()); // 36
