23ba06764ec34cc65bf094328bc0d2d9b4c9f827
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-28 13:11:38 +0300

    Add main.js file for Assignment 07

---
+++ Assignments/From 159 To 168/Assignment 07/main.js
@@ -0,0 +1,35 @@
+/*
+Assignment 07
+You have two Generator Functions.
+The task is to create a Generator Function that will delegate the other Generator Functions.
+You must use what you have learned to make the final function exclude duplicate values.
+See the code to understand the result and the requirements.
+*/
+
+function* genNumbers() {
+  yield* [1, 2, 2, 2, 3, 4, 5];
+}
+function* genLetters() {
+  yield* ["A", "B", "B", "B", "C", "D"];
+}
+
+// Write Your Generator Function Here
+function* genAll() {
+  let uniqueNumbers = [...new Set(genNumbers())];
+  let uniqueLetters = [...new Set(genLetters())];
+
+  yield* uniqueNumbers;
+  yield* uniqueLetters;
+}
+
+let generator = genAll();
+
+console.log(generator.next()); // {value: 1, done: false}
+console.log(generator.next()); // {value: 2, done: false}
+console.log(generator.next()); // {value: 3, done: false}
+console.log(generator.next()); // {value: 4, done: false}
+console.log(generator.next()); // {value: 5, done: false}
+console.log(generator.next()); // {value: "A", done: false}
+console.log(generator.next()); // {value: "B", done: false}
+console.log(generator.next()); // {value: "C", done: false}
+console.log(generator.next()); // {value: "D", done: false}
