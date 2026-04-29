49b26a6c59629dfa549f9506d8e0874d6d1d3be7
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-04-29 15:24:16 +0300

    Add main.js file for Assignment 01

---
+++ Assignments/From 169 To 178/Assignment 01/main.js
@@ -0,0 +1,8 @@
+/*
+Assignment 01
+Create a JSON file named articles.json containing a list of 5 articles.
+Each article should include the article ID, author's name, section, title, and content.
+You can name the elements as you like, following what you've learned previously, and you can insert any random data.
+Help: You can separate each JSON object using a comma (") and place them all inside square brackets [].
+See the example below to understand the concept.
+*/
